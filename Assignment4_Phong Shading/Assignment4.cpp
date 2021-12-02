b600d37b0c8a6158e18caa010d4a058f66288e97
Donghwan Lee <84431962+wkazxf@users.noreply.github.com>
2021-12-02 20:17:15 +0900

    Add files via upload

---
+++ Assignment4_Phong Shading/Assignment4.cpp
@@ -0,0 +1,161 @@
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "j3a.hpp"
+#include "toys.h"
+
+using namespace glm;
+
+void init();
+void render(GLFWwindow* window);
+
+Program program;
+GLuint vertBuf = 0;
+GLuint normBuf = 0;
+GLuint tcoordBuf = 0;
+GLuint triBuf = 0;
+GLuint vertArray = 0;
+
+const float PI = 3.141592;
+
+float theta = 0;
+float phi = 0;
+float fov = 60;
+float cameraDistance = 1;
+
+vec3 cameraPosition = vec3(0, 0, 5);
+vec3 lightPos = vec3(3, 5, 5);
+vec3 lightInt = vec3(1, 1, 1);
+vec3 color = vec3(.3, 1, 1);
+vec3 ambInt = vec3(.1, .1, .1);
+
+double oldX;
+double oldY;
+
+void cursorCallback(GLFWwindow* window, double xpos, double ypos) {
+	int width, height;
+	glfwGetWindowSize(window, &width, &height);
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_1)) {
+		theta -= (xpos - oldX) / width * PI;
+		phi -= (ypos - oldY) / height * PI;
+		if (phi < -PI / 2) phi = -PI / 2;
+		if (phi > PI / 2) phi = PI / 2;
+		if (theta < -PI / 2) theta = -PI / 2;
+		if (theta > PI / 2) theta = PI / 2;
+		printf("%f %f \n", phi, theta);
+	}
+	oldX = xpos;
+	oldY = ypos;
+}
+
+void scrollCallback(GLFWwindow* window, double xoffset, double yoffset) {
+	//cameraDistance = cameraDistance * pow(1,01, yoffset);
+	fov = fov * pow(1.1, yoffset);
+	if (fov < 10) fov = 10;
+	if (fov > 170) fov = 170;
+}
+
+int main(void) {
+	if (!glfwInit()) return -1;
+	GLFWwindow* window = glfwCreateWindow(640, 640, "LeeDongHwanAssignment4", NULL, NULL);
+
+	glfwSetCursorPosCallback(window, cursorCallback);
+	glfwSetScrollCallback(window, scrollCallback);
+
+	glfwMakeContextCurrent(window);
+	glewInit();
+	init();
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		render(window);
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+}
+
+void init() {
+	program.loadShaders("shader.vert", "shader.frag");
+	loadJ3A("Resources/teapot.j3a");
+
+	glGenBuffers(1, &vertBuf);
+	glBindBuffer(GL_ARRAY_BUFFER, vertBuf);
+	glBufferData(GL_ARRAY_BUFFER, nVertices[0] * sizeof(glm::vec3), vertices[0], GL_STATIC_DRAW);
+
+	glGenBuffers(1, &normBuf);
+	glBindBuffer(GL_ARRAY_BUFFER, normBuf);
+	glBufferData(GL_ARRAY_BUFFER, nVertices[0] * sizeof(glm::vec3), normals[0], GL_STATIC_DRAW);
+
+	glGenBuffers(1, &tcoordBuf);
+	glBindBuffer(GL_ARRAY_BUFFER, tcoordBuf);
+	glBufferData(GL_ARRAY_BUFFER, nVertices[0] * sizeof(glm::vec2), texCoords[0], GL_STATIC_DRAW);
+
+	glGenVertexArrays(1, &vertArray);
+	glBindVertexArray(vertArray);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertBuf);
+	glVertexAttribPointer(0, 3, GL_FLOAT, 0, 0, 0);
+
+	glEnableVertexAttribArray(1);
+	glBindBuffer(GL_ARRAY_BUFFER, normBuf);
+	glVertexAttribPointer(1, 3, GL_FLOAT, 0, 0, 0);
+
+	glEnableVertexAttribArray(2);
+	glBindBuffer(GL_ARRAY_BUFFER, tcoordBuf);
+	glVertexAttribPointer(2, 2, GL_FLOAT, 0, 0, 0);
+
+	glGenBuffers(1, &triBuf);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triBuf);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, nTriangles[0] * sizeof(glm::u32vec3), triangles[0], GL_STATIC_DRAW);
+}
+
+void render(GLFWwindow* window) {
+
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+
+	vec3 cameraPos = vec3(rotate(theta, vec3(0, 1, 0)) * rotate(phi, vec3(1, 0, 0)) * vec4(cameraPosition, 1)); // *cameraDistance
+
+	mat4 viewMat = lookAt(cameraPos, vec3(0, 0, 0), vec3(0, 1, 0));
+	mat4 projMat = perspective(fov * PI / 180, width / (float)height, 0.1f, 100.f);
+	mat4 modelMat = mat4(1);
+
+	glViewport(0, 0, width, height);
+	glClearColor(0, 0, .3, 0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_FRAMEBUFFER_SRGB);
+
+	glUseProgram(program.programID);
+
+	GLuint loc;
+	loc = glGetUniformLocation(program.programID, "modelMat");
+	glUniformMatrix4fv(loc, 1, false, glm::value_ptr(modelMat));
+	loc = glGetUniformLocation(program.programID, "viewMat");
+	glUniformMatrix4fv(loc, 1, false, glm::value_ptr(viewMat));
+	loc = glGetUniformLocation(program.programID, "projMat");
+	glUniformMatrix4fv(loc, 1, false, glm::value_ptr(projMat));
+
+	loc = glGetUniformLocation(program.programID, "lightPos");
+	glUniform3fv(loc, 1, glm::value_ptr(lightPos));
+	loc = glGetUniformLocation(program.programID, "lightInt");
+	glUniform3fv(loc, 1, glm::value_ptr(lightInt));
+	loc = glGetUniformLocation(program.programID, "ambInt");
+	glUniform3fv(loc, 1, glm::value_ptr(ambInt));
+	loc = glGetUniformLocation(program.programID, "color");
+	glUniform3fv(loc, 1, glm::value_ptr(color));
+	loc = glGetUniformLocation(program.programID, "cameraPos");
+	glUniform3fv(loc, 1, glm::value_ptr(cameraPos));
+
+
+
+	glBindVertexArray(vertArray);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, triBuf);
+	glDrawElements(GL_TRIANGLES, nTriangles[0] * 3, GL_UNSIGNED_INT, 0);
+	glfwSwapBuffers(window);
+}
---
+++ Assignment4_Phong Shading/shader.frag
@@ -0,0 +1,27 @@
+#version 410 core
+
+out vec4 out_Color;
+
+in vec3 worldPos;
+in vec3 normal;
+in vec2 texCoord;
+
+uniform vec3 lightPos;
+uniform vec3 lightInt;
+uniform vec3 ambInt;
+uniform vec3 color;
+uniform vec3 cameraPos;
+
+void main(void)
+{
+	vec3 L = normalize(lightPos - worldPos);
+	vec3 N = normalize(normal);
+	vec3 R = 2 * dot(N,L) * N - L;
+	vec3 V = normalize(cameraPos - worldPos);
+
+	vec3 diffuse = lightInt * max(0,dot(N,L)) * color; 
+	vec3 ambient = ambInt * diffuse;
+	vec3 specular = lightInt * pow(max(0,dot(R,V)),20); // lightInt = vec3(1,1,1)
+
+	out_Color = vec4(diffuse + ambInt + specular, 1.0);
+}
